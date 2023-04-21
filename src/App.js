c517ca457168973d80e22de9e064c90675bc6f9f
JhonSanz <ingjhonsanz@utp.edu.co>
2023-04-21 14:40:07 -0500

    Added latex library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Plotly from 'plotly.js-gl3d-dist-min';
 import createPlotlyComponent from 'react-plotly.js/factory';
 const Plot = createPlotlyComponent(Plotly);
+var Latex = require('react-latex');
 
 function App() {
   var pointCount = 1000;
@@ -17,17 +15,20 @@ function App() {
   // we generate values for x,y but those are not continuous values
   // and they will look too separated in the chart. If we divide them by a constant
   // are going to be closer, simulating like to be continuous.
- 
+
   for (i = 1; i < pointCount; i++) {
     r = i;
-    x.push(r * Math.cos(i/30));
-    y.push(r * Math.sin(i/30));
+    x.push(r * Math.cos(i / 30));
+    y.push(r * Math.sin(i / 30));
     z.push(i);
     c.push(i)
   }
 
+  const generalForm = "$x^2 + y^2 - z^2 = 0$"
+
   return (
     <div className="App">
+      <Latex>{generalForm}</Latex>
       <Plot
         data={[{
           type: 'scatter3d',
@@ -42,7 +43,7 @@ function App() {
             colorscale: 'Viridis'
           }
         }]}
-      layout={{ width: 1000, height: 900, title: 'Cono circular' }}
+        layout={{ width: 1000, height: 900, title: 'Cono circular' }}
       />
     </div>
   );
