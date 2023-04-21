21597680fbde2cc9d7e2f9208d97aa5d125b5fa3
JhonSanz <ingjhonsanz@utp.edu.co>
2023-04-21 14:00:51 -0500

    Added plotly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Plotly from 'plotly.js-gl3d-dist-min';
+import createPlotlyComponent from 'react-plotly.js/factory';
+const Plot = createPlotlyComponent(Plotly);
 
 function App() {
+  var pointCount = 1000;
+  var i, r;
+
+  var x = [];
+  var y = [];
+  var z = [];
+  var c = [];
+
+  // we generate values for x,y but those are not continuous values
+  // and they will look too separated in the chart. If we divide them by a constant
+  // are going to be closer, simulating like to be continuous.
+ 
+  for (i = 1; i < pointCount; i++) {
+    r = i;
+    x.push(r * Math.cos(i/30));
+    y.push(r * Math.sin(i/30));
+    z.push(i);
+    c.push(i)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plot
+        data={[{
+          type: 'scatter3d',
+          mode: 'lines',
+          x: x,
+          y: y,
+          z: z,
+          opacity: 0.7,
+          line: {
+            width: 10,
+            color: c,
+            colorscale: 'Viridis'
+          }
+        }]}
+      layout={{ width: 1000, height: 900, title: 'Cono circular' }}
+      />
     </div>
   );
 }
