da2e98d11eac025f6b1c74283019b00aef5961b0
JhonSanz <ingjhonsanz@utp.edu.co>
2023-04-27 15:16:05 -0500

    Added overflow scroll

--- src/chartsRepository/taylorPolynomial/description.js
+++ src/chartsRepository/taylorPolynomial/description.js
@@ -7,42 +7,47 @@ const description = <div>
   <p>
     Let's compare <Latex>{"$f(x) = e^{x}$"}</Latex> with <Latex>{"$p(x) = \\displaystyle\\sum_0^k  \\frac{x^k}{k!}$"}</Latex> we are going to derivate these functions and evaluate them in <Latex>{"$x = 0$"}</Latex>. <br />
   </p>
-  <table>
-    <tr>
-      <td>1. </td>
-      <td><Latex>{"$f^{(0)}(x) = e^{x}$"}</Latex></td>
-      <td><Latex>{"$p^{(0)}(x) =  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
-    </tr>
-    <tr>
-      <td>2. </td>
-      <td><Latex>{"$f^{(1)}(x) = e^{x}$"}</Latex></td>
-      <td><Latex>{"$p^{(1)}(x) = 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
-    </tr>
-    <tr>
-      <td>3. </td>
-      <td><Latex>{"$f^{(2)}(x) = e^{x}$"}</Latex></td>
-      <td><Latex>{"$p^{(2)}(x) = 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
-    </tr>
-    <tr>
-      <td>4. </td>
-      <td><Latex>{"$f^{(3)}(x) = e^{x}$"}</Latex></td>
-      <td><Latex>{"$p^{(3)}(x) = 0 + 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
-    </tr>
-  </table>
+  <div style={{ overflowX: "scroll" }}>
+    <table>
+      <tbody>
+        <tr>
+          <td><Latex>{"$f^{(0)}(x) = e^{x} \\ $"}</Latex></td>
+          <td><Latex>{"$p^{(0)}(x) =  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
+        </tr>
+        <tr>
+          <td><Latex>{"$f^{(1)}(x) = e^{x} \\ $"}</Latex></td>
+          <td><Latex>{"$p^{(1)}(x) = 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
+        </tr>
+        <tr>
+          <td><Latex>{"$f^{(2)}(x) = e^{x} \\ $"}</Latex></td>
+          <td><Latex>{"$p^{(2)}(x) = 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
+        </tr>
+        <tr>
+          <td><Latex>{"$f^{(3)}(x) = e^{x} \\ $"}</Latex></td>
+          <td><Latex>{"$p^{(3)}(x) = 0 + 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ...$"}</Latex></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   <p>
     This is great. As you can see all derivates are giving us the same function for both <Latex>{"$f(x)$"}</Latex> and <Latex>{"$p(x)$"}</Latex> I mean: <br /><br />
-    <Latex>{"$f^{(k)}(x) = f(x) \\forall k = 1, 2, 3 ... $"}</Latex> <br />
-    <Latex>{"$p^{(k)}(x) = p(x) \\forall k = 1, 2, 3 ... $"}</Latex>
+    <Latex>{"$f^{(k)}(x) = f(x) \\ \\forall \\ k = 1, 2, 3 ... $"}</Latex> <br />
+    <Latex>{"$p^{(k)}(x) = p(x) \\ \\forall \\ k = 1, 2, 3 ... $"}</Latex>
   </p>
   <p>
     Now let's evaluate  <Latex>{"$x = 0$"}</Latex><br /><br />
-    <Latex>{"$f^{(k)}(0) = 1 \\forall k = 1, 2, 3 ... $"}</Latex> <br />
-    <Latex>{"$p^{(k)}(0) = 1 \\forall k = 1, 2, 3 ... $"}</Latex>
+    <Latex>{"$f^{(k)}(0) = 1 \\ \\forall \\ k = 1, 2, 3 ... $"}</Latex> <br />
+    <Latex>{"$p^{(k)}(0) = 1 \\ \\forall \\ k = 1, 2, 3 ... $"}</Latex>, so: <br />
+    <Latex>{"$p^{(k)}(0) = f^{(k)}(0) \\ \\forall \\ k = 1, 2, 3 ... $"}</Latex>
   </p>
   <p>
-    Those functions are essentially the same. Now in general <br /><br />
-    <Latex>{"$f^{(0)}(0) = p^{(0)}(0), \\ f^{(1)}(0) = p^{(1)}(0), \\ ..., \\ f^{(n)}(0) = p^{(n)}(0)$"}</Latex>
+    Those functions <b>are essentially the same</b>. Now in general, <b>we are trying to find</b> a polynomial <Latex>{"$p$"}</Latex> wich matches with <Latex>{"$f$"}</Latex> and its first <Latex>{"$n$"}</Latex> derivates in <Latex>{"$x = 0$"}</Latex> (as we did previously with <Latex>{"$f(x) = e^{x}$"}</Latex>), so:<br /><br />
+    <Latex>{"$f^{(0)}(0) = p^{(0)}(0)$"}</Latex><br />
+    <Latex>{"$f^{(1)}(0) = p^{(1)}(0)$"}</Latex><br />
+    <Latex>{"$f^{(n)}(0) = p^{(n)}(0) \\ \\forall \\ n = 1, 2, 3 ... $"}</Latex><br />
   </p>
+  <p>The polynomial is... yes, is this:</p>
+  <Latex>{"$p(x) = c_0 + c_1x + c_2x^2 + ... + c_nx^n$"}</Latex><br />
 
 </div>
 
