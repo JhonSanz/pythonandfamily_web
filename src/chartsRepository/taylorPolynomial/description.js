6c480b653e103980839f2a2337205b14193ca2e6
JhonSanz <ingjhonsanz@utp.edu.co>
2023-04-27 10:20:51 -0500

    Added popup description 1

--- src/chartsRepository/taylorPolynomial/description.js
+++ src/chartsRepository/taylorPolynomial/description.js
@@ -1,7 +1,49 @@
 var Latex = require('react-latex');
 
 const description = <div>
-  <p>Have you ever though about how calculators work?. Imagine that you need to compute <Latex>{"$e^{69}$"}</Latex> or </p>
+  <p>
+    Let's retake our <Latex>{"$f(x) = e^{x}$"}</Latex> example. We know that <Latex>{"$f$"}</Latex> and all its derivated terms are <Latex>{"$1$"}</Latex> with <Latex>{"$x = 0$"}</Latex>. That is the key because if we find another function which its derivates are always the same, and its initial conditions are also the same we can say that both are the same functions. Let's look at the next analysis. <br />
+  </p>
+  <p>
+    Let's compare <Latex>{"$f(x) = e^{x}$"}</Latex> with <Latex>{"$p(x) = \\displaystyle\\sum_0^k  \\frac{x^k}{k!}$"}</Latex> we are going to derivate these functions and evaluate them in <Latex>{"$x = 0$"}</Latex>. <br />
+  </p>
+  <table>
+    <tr>
+      <td>1. </td>
+      <td><Latex>{"$f^{(0)}(x) = e^{x}$"}</Latex></td>
+      <td><Latex>{"$p^{(0)}(x) =  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ... +$"}</Latex></td>
+    </tr>
+    <tr>
+      <td>2. </td>
+      <td><Latex>{"$f^{(1)}(x) = e^{x}$"}</Latex></td>
+      <td><Latex>{"$p^{(1)}(x) = 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ... +$"}</Latex></td>
+    </tr>
+    <tr>
+      <td>3. </td>
+      <td><Latex>{"$f^{(2)}(x) = e^{x}$"}</Latex></td>
+      <td><Latex>{"$p^{(2)}(x) = 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ... +$"}</Latex></td>
+    </tr>
+    <tr>
+      <td>4. </td>
+      <td><Latex>{"$f^{(3)}(x) = e^{x}$"}</Latex></td>
+      <td><Latex>{"$p^{(3)}(x) = 0 + 0 + 0 +  1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ... +$"}</Latex></td>
+    </tr>
+  </table>
+  <p>
+    This is great. As you can see all derivates are giving us the same function for both <Latex>{"$f(x)$"}</Latex> and <Latex>{"$p(x)$"}</Latex> I mean: <br /><br />
+    <Latex>{"$f^{(k)}(x) = f(x) \\forall k = 1, 2, 3 ... $"}</Latex> <br />
+    <Latex>{"$p^{(k)}(x) = p(x) \\forall k = 1, 2, 3 ... $"}</Latex>
+  </p>
+  <p>
+    Now let's evaluate  <Latex>{"$x = 0$"}</Latex><br /><br />
+    <Latex>{"$f^{(k)}(0) = 1 \\forall k = 1, 2, 3 ... $"}</Latex> <br />
+    <Latex>{"$p^{(k)}(0) = 1 \\forall k = 1, 2, 3 ... $"}</Latex>
+  </p>
+  <p>
+    Those functions are essentially the same. Now in general <br /><br />
+    <Latex>{"$f^{(0)}(0) = p^{(0)}(0), \\ f^{(1)}(0) = p^{(1)}(0), \\ ..., \\ f^{(n)}(0) = p^{(n)}(0)$"}</Latex>
+  </p>
+
 </div>
 
 
@@ -9,9 +51,11 @@ const miniDescription = <div>
   <p>
     Have you ever though about how calculators work?. Imagine that you need to compute <Latex>{"$e^{69}$"}</Latex> or <Latex>{"$sin(12.5845)$"}</Latex> by hand, <b>what a shit huh?</b>. It is crazy because sometimes we are used to using tools without knowing how they work. This is one of my favorite applications of calculus because despite the fact that we don't have to use it to calculate functions by hand, it is great to know how daily life things work.<br /> <br />
 
-    Here we see a chart of <Latex>{"$e^{x}$"}</Latex> function. This chapter is about <b> approximate non polynomial functions with polynomial ones</b>, and by that way you will be able to compute, because we do know how to perform basic operations <Latex>{"$ + - \\div * $"}</Latex>.  <br /><br />
+    Here we see a chart of <Latex>{"$e^{x}$"}</Latex> function. This chapter is about <b> approximate non polynomial functions with polynomial ones</b>, and by that way you will be able to compute because we do know how to perform basic operations like <Latex>{"$ + - \\div * $"}</Latex>.  <br /><br />
 
-    With these buttons you will be able to add or remove terms <b>of <Latex>{"$e^{x}$"}</Latex> taylor serie</b>. The more terms in our serie, the more accurate is the approximation. It is great to see it becoming more an more similar to our target function.
+    With these buttons you will be able to add or remove terms <b>of <Latex>{"$e^{x}$"}</Latex> taylor serie</b>. The more terms in our serie, the more accurate is the approximation. It is great to see it becoming more an more similar to our target function. <br /><br />
+
+    <Latex>{"$e^x = \\displaystyle\\sum_0^k  \\frac{x^k}{k!} = 1 + \\frac{x^2}{2!} + \\frac{x^3}{3!} + ... + \\frac{x^n}{n!}$"}</Latex>
   </p>
 
 </div>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -15,7 +15,7 @@ const Home = ({ setOpen }) => {
           <CardContent>
             <h3>What is this web about?</h3>
             <p>You will find a lot of things here, nothing that is not in the books. But, that is not certainly the most important. I wanted to expose some math concepts which are sometimes difficult to understand (like it was for me).</p>
-            <p>I will try to cover topics that are great to study with a visual reference. Also, I know that all has to have mathematical strictness, so I will quote references from the book I used to create this website because I'm am not either a mathematician nor a "nerd". I am just a normal human being who wants to learn my myself, things that I could learn in the university.</p>
+            <p>I will try to cover topics that are great to study with a visual reference. Also, I know that all has to have mathematical strictness, so I will quote references from the book I used to create this website because I'm am not either a mathematician nor a "nerd". I am just a normal human being who wants to learn by myself, things that I couldn't learn in the university.</p>
           </CardContent>
         </Card>
       </Grid>
@@ -23,8 +23,8 @@ const Home = ({ setOpen }) => {
         <Card sx={{ minHeight: "310px" }}>
           <CardContent>
             <h3>Why is this important?</h3>
-            <p>Nowadays, artificial intelligence is growing up quickly. Even a lot of people (like me) are getting worry because we are near to lose our jobs because it is so posible that we can be replaced by an algorithm. I know that some artificial intelligence is able to compute 1000X faster than me. Even IA can understand better all these topics than any human.</p>
-            <p>It sounds bad. We can assume that it is not important to learn something new, and it makes kind of sense. But, with the time I realized that learning math goes beyond compute something in your head or approve your test. For me, learning math is a rebellion tool because it forces you to question yourself all the time.</p>
+            <p>Nowadays, artificial intelligence is growing up quickly. Even a lot of people (like me) are getting worry because we are near to lose our jobs because it is so posible that we can be replaced by an algorithm. I know that some artificial intelligence can compute 1000X faster than me. Even IA can understand better all math topics than any human.</p>
+            <p>It sounds bad. We can assume that it is not important to learn something new, and it makes kind of sense. But, with the time I realized that learning math goes beyond computing something in your head or approving your test. For me, learning math is a rebellion tool because it forces you to question yourself all the time.</p>
             <p></p>
           </CardContent>
         </Card>
