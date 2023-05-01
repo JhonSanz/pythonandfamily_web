ad770a1479c63dcbc8e1b9c536469c15dd12f24f
JhonSanz <ingjhonsanz@utp.edu.co>
2023-05-01 15:21:47 -0500

    Added triangular ineguality and proyectos

---
+++ src/chartsRepository/vectors3/description.js
@@ -0,0 +1,11 @@
+var Latex = require('react-latex');
+
+const description = <div>
+  hey :)
+</div>
+
+const miniDescription = <div>
+  Nothing here
+</div>
+
+export { description, miniDescription };
---
+++ src/chartsRepository/vectors3/props.js
@@ -0,0 +1,41 @@
+const generate2dVectors = (x, y, axis, dotted, text) => {
+  return {
+    x,
+    y,
+    xaxis: `x${axis}`,
+    yaxis: `y${axis}`,
+    mode: 'lines+markers+text',
+    type: 'scatter',
+    marker: {
+      symbol: '1'
+    },
+    line: {
+      dash: dotted ? 'dot' : 'solid'
+    },
+    text,
+    textposition: 'top',
+  }
+};
+
+var layout = {
+  grid: { rows: 2, columns: 1, pattern: 'independent', ygap: 0.09 },
+  verticalSpacing: 0.05,
+  showlegend: false
+};
+
+var data = [
+  generate2dVectors([0, 1, 2], [0, 0.75, 1.5], "1", false, ['', '||A+B||', '']),
+  generate2dVectors([0, 0.75, 1.5], [0, 0.25, 0.5], "1", false, ['', '||A||', '']),
+  generate2dVectors([1.5, 1.75, 2], [0.5, 1, 1.5], "1", false, ['', '||B||', '']),
+
+  generate2dVectors([0, 1, 2], [0, 1, 2], "2", false, ['', '', 'A = tB + C']),
+  generate2dVectors([0, 3], [0, 0], "2", false, ['', 'B']),
+  generate2dVectors([2, 2], [0, 2], "2", true, ['tB', '',]),
+  generate2dVectors([0, 0], [0, 2], "2", true, ['', 'C',]),
+];
+
+
+const chartTile = "Triangular inequality and Proyections";
+
+const chartProps = { data, layout, chartTile }
+export default chartProps;
